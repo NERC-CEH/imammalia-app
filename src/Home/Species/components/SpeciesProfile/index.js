bdb62bd6e99e3582e3fbb9f10278adb26314a533
kazlauskis <karolis@kazlauskis.com>
2019-09-26 10:12:00 +0100

    Add species habitats to profiles

--- src/Home/Species/components/SpeciesProfile/index.js
+++ src/Home/Species/components/SpeciesProfile/index.js
@@ -9,17 +9,44 @@ import {
 import PropTypes from 'prop-types';
 import './styles.scss';
 
+const habitats = [
+  'aquatic',
+  'river',
+  'tree',
+  'underground',
+  'forest',
+  'shrubs',
+  'alpine',
+  'prairies',
+  'farmlands',
+];
+
 const Component = ({ species }) => {
+  const habitatsString = habitats
+    .filter(habitat => species[habitat])
+    .map(habitat => t(habitat))
+    .join(', ');
+
   return (
     <IonContent id="species-profile" class="ion-padding">
       <img src={`/images/${species.id}.jpg`} alt="species" />
 
       <IonCardHeader>
-        <IonCardSubtitle>{species.taxon}</IonCardSubtitle>
         <IonCardTitle>{t(species.english)}</IonCardTitle>
+        <IonCardSubtitle>{species.taxon}</IonCardSubtitle>
       </IonCardHeader>
 
-      <IonCardContent>{t(species.description)}</IonCardContent>
+      <IonCardContent>
+        <h3 className="species-label">{`${t('Description')}:`}</h3>
+        {t(species.description)}
+      </IonCardContent>
+
+      {habitatsString && (
+        <IonCardContent className="species-habitats">
+          <h3 className="species-label">{`${t('Habitats')}:`}</h3>
+          {habitatsString}
+        </IonCardContent>
+      )}
     </IonContent>
   );
 };
