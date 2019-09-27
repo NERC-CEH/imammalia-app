e327b4c81d94d2ede20ba306bf5eedccc1d0b1b0
kazlauskis <karolis@kazlauskis.com>
2019-09-27 15:29:05 +0100

    #10 Update warehouse ids

---
+++ src/Home/Species/getWarehouseIDs.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const species = require('./species.data.json');
+
+const getOptions = q => ({
+  method: 'GET',
+  url: 'https://warehouse1.indicia.org.uk/index.php/services/data/taxa_search',
+  qs: {
+    q,
+    limit: '1',
+    orderby: 'taxonomic_sort_order',
+    mode: 'json',
+    sortdir: 'ASC',
+    auth_token: null,
+    nonce: null,
+    taxon_list_id: '258',
+    synonyms: 'false',
+  },
+});
+
+function get(q) {
+  return new Promise(resolve => {
+    request(getOptions(q), function(error, response, body) {
+      if (error) throw new Error(error);
+
+      const res = JSON.parse(body);
+      if (!res || !res[0]) {
+        console.log();
+        resolve();
+        return;
+      }
+
+      const { taxa_taxon_list_id, taxon } = res[0];
+      console.log(`${q}, ${taxon}, ${taxa_taxon_list_id}`);
+      resolve();
+    });
+  });
+}
+
+async function run() {
+  for (let i = 0; i < species.length; i++) {
+    await get(species[i].taxon);
+  }
+}
+
+run();
