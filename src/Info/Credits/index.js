ec6469cf5ace6af272290ea454191de0102d0eff
kazlauskis <karolis@kazlauskis.com>
2019-10-02 15:19:57 +0100

    Improve the looks of credits page

--- src/Info/Credits/index.js
+++ src/Info/Credits/index.js
@@ -8,28 +8,6 @@ export default () => (
   <>
     <AppHeader title={t('Credits')} />
     <IonContent id="credits" class="ion-padding">
-      <IonList lines="none">
-        <IonItem>
-          <IonLabel>
-            {t(
-              'We are very grateful for all the people that helped to create this app:'
-            )}
-          </IonLabel>
-        </IonItem>
-        <ul>
-          <li>
-            <a href="https://www.ceh.ac.uk/staff/david-roy">
-              <b>David Roy</b> (the Centre for Ecology & Hydrology)
-            </a>
-          </li>
-          <li>
-            <a href="https://flumens.io">
-              <b>Karolis Kazlauskis</b> (App developer)
-            </a>
-          </li>
-        </ul>
-      </IonList>
-
       <IonList>
         <IonItem>
           <IonLabel>
@@ -57,22 +35,50 @@ export default () => (
 
       <IonList>
         <IonItem>
-          <IonLabel>{t('Photo credits:')}</IonLabel>
+          <IonLabel>
+            <b>
+              {t(
+                'We are very grateful for all the people that helped to create this app:'
+              )}
+            </b>
+          </IonLabel>
+        </IonItem>
+        <IonItem lines="none">
+          <IonLabel>
+            <a href="https://www.ceh.ac.uk/staff/david-roy">
+              <b>David Roy</b> (the Centre for Ecology & Hydrology)
+            </a>
+          </IonLabel>
         </IonItem>
         <IonItem>
-          {species
-            .filter(s => s.photoAttribution)
-            .map(s => (
-              <li key={s.id}>
-                <IonLabel position="stacked">
-                  <b>{`${s.taxon}: `}</b>
-                  <span
-                    dangerouslySetInnerHTML={{ __html: s.photoAttribution }}
-                  />
-                </IonLabel>
-              </li>
-            ))}
+          <IonLabel>
+            <a href="https://flumens.io">
+              <b>Karolis Kazlauskis</b> (App developer)
+            </a>
+          </IonLabel>
         </IonItem>
+      </IonList>
+
+      <IonList>
+        <IonItem>
+          <IonLabel>
+            <b>{t('Photo credits:')}</b>
+          </IonLabel>
+        </IonItem>
+        {/* <IonItem> */}
+        {species
+          .filter(s => s.photoAttribution)
+          .map(s => (
+            <IonItem key={s.id} lines="none">
+              <IonLabel>
+                <i>{`${s.taxon}: `}</i>
+                <span
+                  dangerouslySetInnerHTML={{ __html: s.photoAttribution }}
+                />
+              </IonLabel>
+            </IonItem>
+          ))}
+        {/* </IonItem> */}
       </IonList>
     </IonContent>
   </>
