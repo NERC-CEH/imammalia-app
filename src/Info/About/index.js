57a94f74f09b63a1097c6a92813e180fff798ed1
kazlauskis <karolis@kazlauskis.com>
2019-10-25 23:42:26 +0100

    Remove 'test' phrase from about page

--- src/Info/About/index.js
+++ src/Info/About/index.js
@@ -18,7 +18,7 @@ const Component = () => (
         <IonItem>
           <IonLabel>
             {t(
-              `The iMammalia App is designed to encourage recording of mammals in the wild. This test version is set up to easily record mammals in four pilot countries: Spain, Germany, Poland and Croatia. Sightings can be recorded from other countries across Europe, but currently the species list is limited to larger mammals found in those four countries.`
+              `The iMammalia App is designed to encourage recording of mammals in the wild. It is set up to easily record mammals in four pilot countries: Spain, Germany, Poland and Croatia. Sightings can be recorded from other countries across Europe, but currently the species list is limited to larger mammals found in those four countries.`
             )}
           </IonLabel>
         </IonItem>
