522c2afc8a83bfaad135d07b2eaa2211dc5ff51b
Karolis Kazlauskis <karolis@flumens.io>
2020-09-24 18:27:41 +0100

    Update excel fetch script

--- src/common/data/index.js
+++ src/common/data/index.js
@@ -1,10 +1,36 @@
-const fetch = require('./fetch');
+const fs = require('fs');
+const fetchSheet = require('@flumens/fetch-onedrive-excel'); // eslint-disable-line
 
-const fileID = '013SAXWCF4PFPFL2BFENA2BSOA3ICVKHSF';
+const drive =
+  'sites/flumensio.sharepoint.com,6230bb4b-9d52-4589-a065-9bebfdb9ce63,21520adc-6195-4b5f-91f6-7af0b129ff5c/drive';
+const file = '01UPL42ZW5AJRZ4FMDSBFL5RGYHDCX4MPK';
+
+function saveSpeciesToFile(data, sheetName) {
+  return new Promise((resolve, reject) => {
+    const fileName = `./${sheetName}.json`;
+    console.log(`Writing ${fileName}`);
+
+    fs.writeFile(fileName, JSON.stringify(data, null, 2), err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(data);
+    });
+  });
+}
 
 (async () => {
-  await fetch(fileID, 'species');
-  await fetch(fileID, 'species_groups');
-  
-  console.log('All done!');
+  await saveSpeciesToFile(
+    await fetchSheet({ drive, file, sheet: 'species' }),
+    'species.data'
+  );
+
+  await saveSpeciesToFile(
+    await fetchSheet({ drive, file, sheet: 'species_groups' }),
+    'species_groups.data'
+  );
+
+  console.log('All done! 🚀');
 })();
