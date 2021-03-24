4e188afca3fcfa4b860482aad14757ecdb87eed3
Vilius Stankaitis <vilius@stankaitis.com>
2021-03-24 10:20:43 +0000

    #83 Update demo app with new languages

--- src/common/helpers/translator.js
+++ src/common/helpers/translator.js
@@ -7,7 +7,12 @@ import hr_HR from '../translations/hr_HR.po';
 import pl_PL from '../translations/pl_PL.po';
 import mk_MK from '../translations/mk_MK.po';
 import sr_RS from '../translations/sr_RS.po';
-
+import sq_AL from '../translations/sq_AL.po';
+import it_IT from '../translations/it_IT.po';
+import bg_BG from '../translations/bg_BG.po';
+import lt_LT from '../translations/lt_LT.po';
+import el_GR from '../translations/el_GR.po';
+import sr_ME from '../translations/sr_ME.po';
 // Adding some context, reference and other in po files:
 
 // #: Some reference!!
@@ -25,6 +30,12 @@ const dictionary = {
   pl_PL,
   mk_MK,
   sr_RS,
+  sq_AL,
+  it_IT,
+  bg_BG,
+  lt_LT,
+  el_GR,
+  sr_ME,
 };
 
 export const languages = {
@@ -35,6 +46,12 @@ export const languages = {
   pl_PL: 'Polski',
   mk_MK: 'Mакедонски',
   sr_RS: 'Cрпски',
+  sq_AL: 'Shqip',
+  it_IT: 'Italiano',
+  bg_BG: 'Български',
+  lt_LT: 'Lietuvių',
+  el_GR: 'Ελληνικά',
+  sr_ME: 'црногорски',
 };
 
 export const countries = {
