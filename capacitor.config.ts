aad4addad835bb0e891b4b48d06f4730153535ff
vilius-stankaitis <vilius@stankaitis.com>
2022-09-13 15:40:52 +0300

    Fix android/ios capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'uk.ac.ceh.imammalia',
+  appName: 'iMammalia App',
+  bundledWebRuntime: false,
+  webDir: 'build',
+};
+
+export default config;
