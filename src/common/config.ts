976ab5f2e39aa789ca9b3430ac5ca70c845f0652
vilius-stankaitis <vilius@stankaitis.com>
2022-09-16 10:15:06 +0300

    Update config

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,42 +1,38 @@
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { isPlatform } from '@ionic/react';
 
-// const backendUrl = process.env.APP_BACKEND_URL || 'https://irecord.org.uk';
+const backendUrl =
+  process.env.APP_BACKEND_URL || 'https://european-mammals.brc.ac.uk';
 
-// const indiciaUrl =
-//   process.env.APP_BACKEND_INDICIA_URL || 'https://warehouse1.indicia.org.uk';
-
-// const backendUrl =
-//   process.env.APP_BACKEND_URL || 'https://european-mammals.brc.ac.uk/';
-
-const backendUrl = 'https://european-mammals.brc.ac.uk/';
+const indiciaUrl =
+  process.env.APP_BACKEND_INDICIA_URL || 'https://warehouse1.indicia.org.uk';
 
 const config = {
-  environment: 'process.env.NODE_ENV as string',
-  version: 'process.env.APP_VERSION as string',
-  build: 'process.env.APP_BUILD as string',
+  environment: process.env.NODE_ENV as string,
+  version: process.env.APP_VERSION as string,
+  build: process.env.APP_BUILD as string,
   feedbackEmail: 'apps%40ceh.ac.uk',
 
-  sentryDNS: 'process.env.APP_SENTRY_KEY as string',
+  sentryDNS: process.env.APP_SENTRY_KEY as string,
 
   map: {
-    mapboxApiKey: 'process.env.APP_MAPBOX_MAP_KEY as string',
+    mapboxApiKey: process.env.APP_MAPBOX_MAP_KEY as string,
     mapboxSatelliteId: 'cehapps/cipqvo0c0000jcknge1z28ejp',
   },
 
+  mapbox_api_key: process.env.APP_MAPBOX_MAP_KEY,
+
   backend: {
     url: backendUrl,
-    websiteId: -1,
-    clientId: 'process.env.APP_BACKEND_CLIENT_ID as string',
-    clientPass: 'process.env.APP_BACKEND_CLIENT_PASS as string',
+    websiteId: 118,
+
+    clientId: process.env.APP_BACKEND_CLIENT_ID as string,
+    clientPass: process.env.APP_BACKEND_CLIENT_PASS as string,
 
     indicia: {
-      url: '',
+      url: indiciaUrl,
     },
   },
-
-  weatherSiteApiKey: 'process.env.APP_WEATHER_SITE_API_KEY as string',
-  weatherSiteUrl: 'https://api.openweathermap.org/data/2.5/weather',
 
   dataPath: '',
 };
---
+++ src/index.tsx
@@ -0,0 +1,56 @@
+import ReactDOM from 'react-dom';
+import { setupIonicReact } from '@ionic/react';
+import config from 'common/config';
+import { configure as mobxConfig } from 'mobx';
+import appModel from 'models/app';
+import userModel from 'models/user';
+import savedSamples from 'models/savedSamples';
+import { initAnalytics, device } from '@flumens';
+import App from './App';
+import '@capacitor/core';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import 'common/theme.scss';
+
+console.log('🚩 App starting.');
+
+setupIonicReact({
+  hardwareBackButton: false,
+  swipeBackEnabled: false,
+});
+
+mobxConfig({ enforceActions: 'never' });
+
+async function init() {
+  await appModel.ready;
+  await userModel.ready;
+  await savedSamples._init;
+
+  appModel.attrs.sendAnalytics &&
+    initAnalytics({
+      dsn: config.sentryDNS,
+      environment: config.environment,
+      build: config.build,
+      release: config.version,
+      userId: userModel.id,
+      tags: {
+        'app.appSession': appModel.attrs.appSession,
+      },
+    });
+
+  appModel.attrs.appSession += 1;
+  appModel.save();
+
+  if (userModel.attrs.password && device.isOnline) {
+    // TODO: remove after this propagates to all users
+    try {
+      userModel._migrateAuth();
+    } catch (_) {
+      // do nothing
+    }
+  }
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+init();
