5d15340254a0d1d0eef3a80ce698445cfc2e5a4c
vilius-stankaitis <vilius@stankaitis.com>
2022-09-16 10:32:07 +0300

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,16 @@ const webpack = require('webpack');
 const appConfig = require('@flumens/webpack-config');
 
 const required = [
+  'APP_BACKEND_CLIENT_ID',
+  'APP_BACKEND_CLIENT_PASS',
   'APP_MAPBOX_MAP_KEY',
   'APP_SENTRY_KEY',
   'APP_INDICIA_API_KEY',
 ];
 
 const development = {
-  APP_INDICIA_API_HOST: '',
+  APP_BACKEND_INDICIA_URL: '',
+  APP_BACKEND_URL: '',
 };
 
 appConfig.plugins.unshift(
